0a18985c8a4624ce2915d9646f3ebd7666056475
suhasgaikwad8902 <suhassureshgaikwad@gmail.com>
2024-08-23 11:38:21 +0530

    new changes

--- app.py
+++ app.py
@@ -310,8 +310,8 @@ if st.button('Analyze PDF and Generate Reports', key='process', help="Click to p
     if pdf_file is not None:
         # Define the path to save the uploaded PDF in the static folder
         k = 1
-        save_path = os.path.join(current_dir , 'static', str(k))
-
+        save_path = os.path.join(current_dir , 'static', str(k)+".pdf")
+        save_path
         # Save the uploaded PDF to the static folder
         with open(save_path, "wb") as f:
             f.write(pdf_file.read())
